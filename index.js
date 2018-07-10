1b7b5b6ef09f017455bc975dbff3a6b51ae5da20
Schuyler Lindberg <schuyberg@gmail.com>
2018-07-10 14:03:38 -0700

    EXIF dependency

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require("./lib/dropzone.js"); // Exposing dropzone
+// let EXIF = require('exif-js');
+// module.exports = require("./lib/dropzone.js"); // Exposing dropzone
+module.exports = define(["./lib/dropzone.js", 'exif-js'], function(dropzone, EXIF) {
+  return dropzone;
+});
